c98d7088fb6d238ac9a59fa822e41151b5ee5dd5
Mark Lee <mark@markelee.com>
2015-10-31 19:19:07 -0400

    Build the initrd with squashfs support

--- build-scripts/build-squashed-root.sh
+++ build-scripts/build-squashed-root.sh
@@ -3,14 +3,26 @@
 # create the read only squashed root
 
 
-## set the user specified packages from ../squashed-packages.txt
+## set the user specified packages from squashed-packages.txt
 packages=()
 while read -r; do
   packages+=($REPLY)
-done < ../squashed-packages.txt
+done < squashed-packages.txt
 
 ## requires arch-install-scripts pacman
 ## install arch linux to a directory
 mkdir baseOS-squashed-root
 pacstrap baseOS-squashed-root base ${packages[@]}
+
+## copy baseOS-squashfs hooks to the new arch linux install
+cp initcpio-hooks/hooks/squashfs baseOS-squashed-root/usr/lib/initpcio/hooks/
+cp initcpio-hooks/install/squashfs baseOS-squashed-root/usr/lib/initcpio/install/
+
+## copy mkinitcpio.conf to the new arch linux install
+cp mkinitcpio.conf baseOS-squashed-root/etc/
+
+## rebuild initramfs in the new arch linux install
+arch-chroot baseOS-squashed-root 'mkinitcpio -p linux'
+
+## squashed the baseOS directory
 mksquashfs baseOS-squashed-root baseOS.sqsh
