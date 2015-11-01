50e463b28b44ae7a6c6c16ccaca9ad95760f28af
Mark Lee <mark@markelee.com>
2015-10-31 21:12:16 -0400

    Fixed up polling issues of user-crypt.img

--- build-scripts/build-user-crypt-img.sh
+++ build-scripts/build-user-crypt-img.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# build the user encrypted img for storing custom settings
+# build the user encrypted img for storing custom setting
+
 fallocate -l 500M user-crypt.img
 cryptsetup luksFormat user-crypt.img
-cryptsetup open user-crypt.img
+cryptsetup open user-crypt.img user-crypt.img
+sleep 1s
 mkfs.ext4 /dev/mapper/user-crypt.img
+sleep 1s
 cryptsetup close user-crypt.img
