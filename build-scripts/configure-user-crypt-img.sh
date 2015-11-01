b556bdac27e4f5cd59101aad632f8eda39ffbcbb
Mark Lee <mark@markelee.com>
2015-11-01 10:26:38 -0500

    Fix up mounting issue. Allow running auto configure script

--- build-scripts/configure-user-crypt-img.sh
+++ build-scripts/configure-user-crypt-img.sh
@@ -3,14 +3,20 @@
 # Overlay the user-crypt-img onto the squashfs-root directory
 
 ## open the user-crypt img
-cryptsetup open user-crypt.img user-crypt
+cryptsetup open user-crypt.img user-crypt.img
 
 ## make needed overlay directories
 mkdir baseOS-work
 mkdir baseOS-root
 
 ## overlay user-crypt.img with baseOS-squashfs-root
-mount baseOS-root -o lowerdir=baseOS-squashed-root,upperdir=/dev/mapper/user-crypt,workdir=baseOS-work baseOS-root
+mount baseOS-root -t overlay -o lowerdir=baseOS-squashed-root,upperdir=/dev/mapper/user-crypt,workdir=baseOS-work baseOS-root
 
 ## chroot into the new directory
-arch-chroot baseOS-root
+## check if auto configuration was passsed
+if [ ! "$1" = "auto" ]; then
+   arch-chroot baseOS-root
+   umount baseOS-root
+   rm -r baseOS-work baseOS-root
+   cryptsetup close user-crypt.img
+fi
