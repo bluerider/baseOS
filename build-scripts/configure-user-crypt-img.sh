4e79d75d9df9dbdc8c29b9b5bd1a7647eb31accc
Mark Lee <mark@markelee.com>
2015-11-02 20:11:37 -0500

    Fix up mounting issues with configuring the user img.

--- build-scripts/configure-user-crypt-img.sh
+++ build-scripts/configure-user-crypt-img.sh
@@ -5,18 +5,25 @@
 ## open the user-crypt img
 cryptsetup open user-crypt.img user-crypt.img
 
+## set up encrypted img
+mkdir -p baseOS-crypt
+mount /dev/mapper/user-crypt.img baseOS-crypt
+mkdir baseOS-crypt/{work,upper}
+
 ## make needed overlay directories
-mkdir baseOS-work
 mkdir baseOS-root
 
 ## overlay user-crypt.img with baseOS-squashfs-root
-mount baseOS-root -t overlay -o lowerdir=baseOS-squashed-root,upperdir=/dev/mapper/user-crypt,workdir=baseOS-work baseOS-root
+mount baseOS-root -t overlay -o lowerdir=baseOS-squashed-root,upperdir=baseOS-crypt/upper,workdir=baseOS-crypt/work
 
 ## chroot into the new directory
 ## check if auto configuration was passsed
 if [ ! "$1" = "auto" ]; then
    arch-chroot baseOS-root
    umount baseOS-root
+   rm -r baseOS-crypt/work
+   mv baseOS-crypt/upper/* baseOS-crypt
+   rm -r baseOS-crypt/upper
    rm -r baseOS-work baseOS-root
    cryptsetup close user-crypt.img
 fi
